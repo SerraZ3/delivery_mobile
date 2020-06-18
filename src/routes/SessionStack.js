70113ceec67d548b2baa4ab2f7b138ee11faca93
SerraZ3 <serra.henrique3@gmail.com>
2020-06-17 22:38:19 -0300

    :wolf: Added authentication for login

--- src/routes/SessionStack.js
+++ src/routes/SessionStack.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
 import Login from '../views/Login';
 import ClientTab from './ClientTab';
@@ -6,15 +7,44 @@ import ClientTab from './ClientTab';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import api from '../services/api';
+
 const Stack = createStackNavigator();
 
-const SessionStack = () => (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Login" headerMode="none">
-      <Stack.Screen name="ClientTab" component={ClientTab} />
-      <Stack.Screen name="Login" component={Login} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const SessionStack = () => {
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+
+  function addUser(user) {
+    dispatch({type: 'ADD_USER', user});
+  }
+  useEffect(() => {
+    const isAuthenticated = async () => {
+      try {
+        let user = (await api.get('/client/user/', {})).data;
+        addUser({
+          ...user.person,
+          email: user.email,
+          roles: user.roles,
+          permission: user.permission,
+          id: user.id,
+        });
+      } catch (error) {}
+    };
+    if (token) isAuthenticated();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none">
+        {token ? (
+          <Stack.Screen name="ClientTab" component={ClientTab} />
+        ) : (
+          <Stack.Screen name="Login" component={Login} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default SessionStack;
