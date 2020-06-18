8b31f8eb150c1bd5ad66b5b1663778428a5a2648
SerraZ3 <serra.henrique3@gmail.com>
2020-06-17 22:36:46 -0300

    :wolf: Added redux and redux persist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Session from './routes/SessionStack';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+
+import {store, persistor} from './store';
 
 const App = () => {
-  return <Session />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Session />
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default App;
