881bd9081d00fd0678cd1612e8e5847c506767f5
SerraZ3 <serra.henrique3@gmail.com>
2020-06-17 22:37:52 -0300

    :wolf: Added interceptor axios for authentication

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,21 @@
+import React from 'react';
 import axios from 'axios';
+import {useSelector} from 'react-redux';
+import {store} from '../store';
 
-export const url_api = 'http://192.168.25.5:8000/v1/api/auth/login'; // Em casa
-
-// import {getToken} from './auth';
+export const url_api = 'http://192.168.25.5:8000/v1/api/'; // Em casa
 
 const api = axios.create({
   baseURL: url_api,
 });
 
-// api.interceptors.request.use(async (config) => {
-//   // const token = getToken();
-//   const token =
-//     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE0LCJpYXQiOjE1OTEyNDg4NDF9.PMPgE7EhuikX5lUcWopu4fEFXRZttnygDIdUu-ROjX8';
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+api.interceptors.request.use(async (config) => {
+  const token = store.getState().auth.token;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
