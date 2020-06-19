cd1e963dad4ad507f726433700ee2d483429681f
SerraZ3 <serra.henrique3@gmail.com>
2020-06-18 23:26:42 -0300

    :wolf: Added product stack in tab

--- src/routes/ClientTab.js
+++ src/routes/ClientTab.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
 
-import Login from '../views/Login';
-import Home from '../views/Home';
+import Order from '../views/Order';
+import Profile from '../views/Profile';
+import ProductStack from './ProductStack';
+import IconWithBadge from '../components/IconWithBadge';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
@@ -15,32 +16,6 @@ import {
 
 const Tab = createBottomTabNavigator();
 
-function IconWithBadge({name, badgeCount, color, size}) {
-  return (
-    <View style={{width: 24, height: 24, margin: 5}}>
-      <Ionicons name={name} size={size} color={color} />
-      {badgeCount > 0 && (
-        <View
-          style={{
-            // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
-            position: 'absolute',
-            right: -6,
-            top: -3,
-            backgroundColor: 'red',
-            borderRadius: 6,
-            width: 12,
-            height: 12,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text style={{color: 'white', fontSize: 10, fontWeight: 'bold'}}>
-            {badgeCount}
-          </Text>
-        </View>
-      )}
-    </View>
-  );
-}
 export default function App() {
   return (
     <Tab.Navigator
@@ -62,14 +37,7 @@ export default function App() {
           }
           if (route.name === 'Pedidos') {
             // You can return any component that you like here!
-            return (
-              <IconWithBadge
-                name={iconName}
-                size={size}
-                color={color}
-                badgeCount={1}
-              />
-            );
+            return <IconWithBadge name={iconName} size={size} color={color} />;
           }
 
           // You can return any component that you like here!
@@ -80,9 +48,9 @@ export default function App() {
         activeTintColor: PRIMARY_COLOR_DARK,
         inactiveTintColor: PRIMARY_COLOR_DARK_TRANSPARENT,
       }}>
-      <Tab.Screen name="Produtos" component={Home} />
-      <Tab.Screen name="Pedidos" component={Login} />
-      <Tab.Screen name="Conta" component={Home} />
+      <Tab.Screen name="Produtos" component={ProductStack} />
+      <Tab.Screen name="Pedidos" component={Order} />
+      <Tab.Screen name="Conta" component={Profile} />
     </Tab.Navigator>
   );
 }
