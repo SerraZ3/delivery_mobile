2cb74a51b12887b91cd153a04560f7ac3017cb31
SerraZ3 <serra.henrique3@gmail.com>
2020-06-18 23:30:05 -0300

    :wolf: Added combine reducers

--- src/store/index.js
+++ src/store/index.js
@@ -5,51 +5,27 @@ import {combineReducers} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 // Config para persistencia de dados com o redux
-import {persistStore, persistReducer} from 'redux-persist';
-
-// Para armazenamento dos dados no celular
-import AsyncStorage from '@react-native-community/async-storage';
+import {persistStore} from 'redux-persist';
 
 // Logger para o redux
 import {createLogger} from 'redux-logger';
 
-import user from './reducer/user';
-
+import reducer from './reducer';
 // Array que conterá todos os middlewares
 const middlewares = [];
 
 // Cria middleware para o saga
-const sagaMiddleware = createSagaMiddleware();
+// const sagaMiddleware = createSagaMiddleware();
 
 // Logger para redux
 const logger = createLogger({});
 
-// Cria uma config com chave para a persistencia de dados
-const rootPersistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  timeout: null,
-};
-
-// Cria uma config com chave para a persistencia de dados
-const userPersistConfig = {
-  key: 'user',
-  storage: AsyncStorage,
-  timeout: null,
-};
-
-// Combina todos os reducers
-const allReducers = combineReducers({
-  user: persistReducer(userPersistConfig, user),
-});
-
 middlewares.push(logger);
 
-const store = createStore(
-  persistReducer(userPersistConfig, user),
-  applyMiddleware(...middlewares),
-);
+// Criar o store com os reducers
+const store = createStore(reducer, applyMiddleware(...middlewares));
 
+// Criar o persistor com a store
 const persistor = persistStore(store);
 
 export {store, persistor};
