3c081842ddb4e5864c12d71aef5de26afd5ca59a
SerraZ3 <serra.henrique3@gmail.com>
2020-07-14 03:40:37 -0300

    :wolf: Added function to get wallpaper and convert date less then 10

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,1 +1,10 @@
 export const toFloat = (value) => parseFloat(parseFloat(value).toFixed(2));
+export const getWallpaper = (images) =>
+  images.find((val) => val.pivot.wallpaper);
+
+export const dateLess10 = (number) => {
+  if (number < 10) {
+    number = '0' + number;
+  }
+  return number;
+};
