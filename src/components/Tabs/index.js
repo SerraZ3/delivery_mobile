40ddd4a7daa4bf3e75650af865ce876cc5522a21
SerraZ3 <serra.henrique3@gmail.com>
2020-07-16 02:13:09 -0300

    :wolf: Added listItensHorizontal and Header in flatlist

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,73 +1,43 @@
 import React from 'react';
-import {ActivityIndicator, FlatList, TouchableHighlight} from 'react-native';
-import {Image} from 'react-native-elements';
+import {FlatList} from 'react-native';
 
-import {
-  Container,
-  TabsContainer,
-  TabItem,
-  TabPrice,
-  TabTitle,
-  Title,
-} from './styles';
-import {PRIMARY_COLOR_TRANSPARENT} from 'react-native-dotenv';
+import {Container, TabsContainer, Title} from './styles';
 
-const ListItens = ({product, count, navigation, establishment}) => {
-  const openProduct = (id) => {
-    console.log(establishment);
-    establishment
-      ? navigation.navigate('EstablishmentProduct', {id})
-      : navigation.navigate('Product', {id});
-  };
-  return product.map((value, idx) => {
-    return (
-      <TouchableHighlight
-        onPress={() => openProduct(value.id)}
-        underlayColor="white"
-        key={idx}>
-        <TabItem
-          style={{
-            shadowColor: PRIMARY_COLOR_TRANSPARENT,
-            shadowOffset: {
-              width: 0,
-              height: 1,
-            },
-            shadowOpacity: 0.2,
-            shadowRadius: 1.41,
-            elevation: 1,
-          }}>
-          <Image
-            source={{uri: value.images[0].url}}
-            style={{width: 50, height: 50}}
-            PlaceholderContent={<ActivityIndicator />}
-          />
-          <TabTitle>{value.name}</TabTitle>
-          <TabPrice>{`R$ ${value.price.replace('.', ',')}`}</TabPrice>
-        </TabItem>
-      </TouchableHighlight>
-    );
-  });
-};
+import ListItensHorizontal from '../ListItensHorizontal';
 
-const Tabs = ({products, navigation, establishment}) => (
+const Tabs = ({products, navigation, establishment, topProducts, ranking}) => (
   <FlatList
     data={products}
     keyExtractor={(item) => item.id.toString()}
-    renderItem={({item, index}) => {
+    renderItem={({item}) => {
+      console.log(item);
       return item.products.length > 0 ? (
         <Container>
           <Title>{item.name}</Title>
           <TabsContainer>
-            <ListItens
+            <ListItensHorizontal
               establishment={establishment}
               product={item.products}
-              count={index}
               navigation={navigation}
             />
           </TabsContainer>
         </Container>
       ) : null;
     }}
+    ListHeaderComponent={
+      topProducts ? (
+        <Container>
+          <Title>Os 10 mais pedidos por vocês!</Title>
+          <TabsContainer>
+            <ListItensHorizontal
+              product={topProducts}
+              navigation={navigation}
+              ranking
+            />
+          </TabsContainer>
+        </Container>
+      ) : null
+    }
   />
 );
 
--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 import {ScrollView, Text, View} from 'react-native';
 
-import {PRIMARY_COLOR, PRIMARY_COLOR_TRANSPARENT} from 'react-native-dotenv';
+import {PRIMARY_COLOR} from 'react-native-dotenv';
 
 export const Container = styled(View)({
   height: 180,
@@ -13,28 +13,6 @@ export const TabsContainer = styled(ScrollView).attrs({
   contentContainerStyle: {paddingLeft: 10, paddigRight: 20},
   showsHorizontalScrollIndicator: false,
 })({});
-
-export const TabItem = styled(View)({
-  width: 110,
-  height: 140,
-  backgroundColor: 'white',
-  borderRadius: 5,
-  marginLeft: 10,
-  padding: 10,
-  justifyContent: 'space-between',
-  alignItems: 'center',
-});
-
-export const TabPrice = styled(Text)({
-  fontSize: 13,
-  color: PRIMARY_COLOR,
-});
-export const TabTitle = styled(Text)({
-  fontSize: 13,
-  color: 'black',
-  fontWeight: 'bold',
-  textAlign: 'center',
-});
 
 export const Title = styled(Text)({
   fontSize: 18,
