73c35238b30b35cdc1962e95fd2391d903b3b084
SerraZ3 <serra.henrique3@gmail.com>
2020-07-16 03:03:28 -0300

    :wolf: Added delay to show

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 
 import {RED_COLOR} from 'react-native-dotenv';
 
 const NotFound = ({message}) => {
-  return (
+  const [delay, setDelay] = useState(false);
+  useEffect(() => {
+    let mounted = true;
+    const runDelay = async () => {
+      if (mounted) {
+        setTimeout(() => {
+          setDelay(true);
+        }, 2000);
+      }
+    };
+    if (!delay) runDelay();
+    return () => (mounted = false);
+  }, []);
+  return delay ? (
     <View style={{alignItems: 'center', marginTop: 20}}>
       <Text style={{color: RED_COLOR, fontSize: 18}}>
         {message ? message : 'Nenhum item encontrado'}
       </Text>
     </View>
-  );
+  ) : null;
 };
 
 export default NotFound;
